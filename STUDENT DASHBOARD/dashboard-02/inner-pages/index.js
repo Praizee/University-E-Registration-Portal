e8927c6f7f46c993411a9f79fd2b0a83192f6a46
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-04-23 09:51:54 +0100

    Update index.js

--- STUDENT DASHBOARD/dashboard-02/inner-pages/index.js
+++ STUDENT DASHBOARD/dashboard-02/inner-pages/index.js
@@ -1,1 +1,12 @@
 //Hey there!
+
+
+// show/hide password
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
